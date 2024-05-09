342bf71816d90a87ef36d8b2889d134aeacbb918
jake <jake@gravylab.co.kr>
2024-05-09 01:59:58 +0000

    script file update for deepspeed multi gpu

--- examples/sft/run_peft_qlora_deepspeed_stage3.sh
+++ examples/sft/run_peft_qlora_deepspeed_stage3.sh
@@ -1,21 +1,28 @@
-accelerate launch --config_file "configs/deepspeed_config_z3_qlora.yaml"  train.py \
+#!/bin/bash
+
+model_name="yanolja/EEVE-Korean-Instruct-10.8B-v1.0"
+table_names="alignment_table, chat_table"
+logging_steps=1000
+
+accelerate launch --config_file "peft/examples/sft/configs/deepspeed_config_z3_qlora.yaml"  peft/examples/sft/train.py \
 --seed 100 \
---model_name_or_path "meta-llama/Llama-2-70b-hf" \
---dataset_name "smangrul/ultrachat-10k-chatml" \
+--model_name_or_path $model_name \
+--database_table_name "alignment_table" \
+--database_url "postgresql://postgres:kk3249@114.110.135.85?port=5432&dbname=alignment" \
 --chat_template_format "chatml" \
 --add_special_tokens False \
 --append_concat_token False \
 --splits "train,test" \
---max_seq_len 2048 \
+--max_seq_len 4096 \
 --num_train_epochs 1 \
---logging_steps 5 \
+--logging_steps $logging_steps \
 --log_level "info" \
 --logging_strategy "steps" \
---evaluation_strategy "epoch" \
---save_strategy "epoch" \
---push_to_hub \
---hub_private_repo True \
---hub_strategy "every_save" \
+--evaluation_strategy "steps" \
+--eval_steps $logging_steps \
+--logging_steps $logging_steps \
+--save_steps $logging_steps \
+--warmup_steps 500 \
 --bf16 True \
 --packing True \
 --learning_rate 1e-4 \
@@ -23,10 +30,10 @@ accelerate launch --config_file "configs/deepspeed_config_z3_qlora.yaml"  train.
 --weight_decay 1e-4 \
 --warmup_ratio 0.0 \
 --max_grad_norm 1.0 \
---output_dir "llama-sft-qlora-dsz3" \
---per_device_train_batch_size 2 \
---per_device_eval_batch_size 2 \
---gradient_accumulation_steps 2 \
+--output_dir "outputs/"$model_name \
+--per_device_train_batch_size 1 \
+--per_device_eval_batch_size 1 \
+--gradient_accumulation_steps 8 \
 --gradient_checkpointing True \
 --use_reentrant True \
 --dataset_text_field "content" \
@@ -39,4 +46,5 @@ accelerate launch --config_file "configs/deepspeed_config_z3_qlora.yaml"  train.
 --use_4bit_quantization True \
 --use_nested_quant True \
 --bnb_4bit_compute_dtype "bfloat16" \
---bnb_4bit_quant_storage_dtype "bfloat16"
+--bnb_4bit_quant_storage_dtype "bfloat16" \
+--use_flash_attn True
